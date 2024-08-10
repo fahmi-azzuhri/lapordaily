c937773b43d69f5b2dc226e2caaff533066d95b7
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-08-10 23:16:24 +0700

    delete toast, fix role

--- backend/prisma/migrations/20240807142606_change_structure/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE `laporanpekerjaan` DROP FOREIGN KEY `LaporanPekerjaan_userId_fkey`;
-
--- AlterTable
-ALTER TABLE `laporanpekerjaan` ALTER COLUMN `tanggal` DROP DEFAULT,
-    MODIFY `userId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `LaporanPekerjaan` ADD CONSTRAINT `LaporanPekerjaan_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;

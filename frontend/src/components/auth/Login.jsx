ec3d343edeae88e20c0a44c795961e0d69c76460
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-08-10 21:34:07 +0700

    add toast login

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import ViewLogin from "../../views/auth/ViewLogin";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+
 export function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
@@ -20,20 +22,26 @@ export function Login() {
       const expires = new Date(new Date().getTime() + 60 * 60 * 1000);
       Cookies.set("token", token, { expires, secure: true });
       Cookies.set("username", username);
-      if (role === "ADMIN") {
-        navigate("/admin/dashboard");
-      } else {
-        navigate("/user/input");
-      }
+      toast.success("Login Berhasil", {
+        style: {
+          padding: "9px",
+          borderRadius: "10px",
+        },
+      });
+      setTimeout(() => {
+        if (role === "ADMIN") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/input");
+        }
+      }, 3000);
     } catch (error) {
-      console.error(
-        "Login error:",
-        error.response ? error.response.data : error.message
-      );
+      toast.error("Login Gagal");
     }
   };
   return (
     <section className="grid text-center h-screen items-center p-8">
+      <Toaster position="top-right" reverseOrder={false} />
       <ViewLogin
         username={username}
         setUsername={setUsername}

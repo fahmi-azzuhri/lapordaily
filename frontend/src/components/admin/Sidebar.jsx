5a185b69fcb45b7c5de5ed79d168f8539b0ed9aa
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-08-15 09:08:52 +0700

    add logout admin dashboard

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function Sidebar() {
   const [activeTab, setActiveTab] = useState("laporan");
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    navigate("/");
+    setActiveTab("logout");
+  };
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    if (tab === "laporan") {
+      navigate("/"); // Navigasi ke halaman dashboard
+    }
+  };
 
   return (
     <div className="w-64 h-screen bg-gray-800 text-white p-5">
       <h1 className="text-xl font-bold mb-6">Dashboard</h1>
       <ul>
         <li
-          className={`mb-4 ${
-            activeTab === "laporan" ? "text-white" : "text-gray-300"
+          className={`mb-4 px-4 py-2 rounded-md cursor-pointer ${
+            activeTab === "laporan"
+              ? "bg-white text-gray-900"
+              : "hover:bg-white hover:text-gray-900"
           }`}
+          onClick={() => handleTabClick("laporan")}
         >
-          <a
-            href="#"
-            className={`hover:text-white ${
-              activeTab === "laporan" ? "font-bold" : ""
-            }`}
-            onClick={() => setActiveTab("laporan")}
-          >
-            Laporan
-          </a>
+          Laporan
+        </li>
+        <li
+          className={`mb-4 px-4 py-2 rounded-md cursor-pointer ${
+            activeTab === "logout"
+              ? "bg-white text-gray-900"
+              : "hover:bg-white hover:text-gray-900"
+          }`}
+          onClick={handleLogout}
+        >
+          Keluar
         </li>
       </ul>
     </div>

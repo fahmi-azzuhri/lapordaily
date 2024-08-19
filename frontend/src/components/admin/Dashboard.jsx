e36be0b72b808bd505a79a9df9d75ab2e72b5681
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-08-19 22:00:25 +0700

    responsive mobile dashboard admin

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -18,8 +18,8 @@ function Dashboard() {
   }, []);
 
   return (
-    <div className="flex-grow p-6">
-      <table className="min-w-full bg-white border border-gray-200">
+    <div className="flex-grow p-6 overflow-x-auto">
+      <table className="min-w-full bg-white border border-gray-200 text-sm">
         <thead>
           <tr className="bg-gray-700 text-white ">
             <th className="border px-4 py-2">Tanggal</th>

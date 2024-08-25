d2ded463b92906b3041936bc688499393c6af5d0
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-08-25 14:55:06 +0700

    delete docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/",
   plugins: [react()],
-  preview : {
-    port : 4000,
-    strictPort : true
-  },
-  server : {
-    port : 4000,
-    strictPort : true,
-    host : true,
-    origin : "http://0.0.0.0:4000"
-  }
 });

05baddf17280ee9a5b8615017bc6c00583289e16
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-09-01 21:46:38 +0700

    consume api get all users

--- frontend/src/components/admin/DataAnggota.jsx
+++ frontend/src/components/admin/DataAnggota.jsx
@@ -1,6 +1,20 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function DataAnggota() {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/users");
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Terjadi kesalahan:", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <h1>Data Anggota</h1>
